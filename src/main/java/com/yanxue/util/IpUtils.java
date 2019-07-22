695c0e772cd010354d3535b2ecd962225eaeab02
yuan <sa517294@mail.ustc.edu.cn>
2019-07-22 17:57:30 +0800

    revise IpUtils.java

--- src/main/java/com/yanxue/util/IpUtils.java
+++ src/main/java/com/yanxue/util/IpUtils.java
@@ -5,6 +5,7 @@
 
 package com.yanxue.util;
 
+import java.util.Arrays;
 import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,33 +22,31 @@ public class IpUtils {
         logger.info("request: {}", request);
         String ip = request.getHeader("x-forwarded-for");
         logger.info("x-forwarded-for ip: {}", ip);
-        if (StringUtils.isNotBlank(ip) && !"unknown".equalsIgnoreCase(ip)) {
+        if (StringUtils.isNotBlank(ip) && !"unknown".equalsIgnoreCase(ip) && isVaildIpAddr(ip)) {
             // 多次反向代理后会有多个ip值，第一个ip才是真实ip
-            if (ip.indexOf(",") != -1) {
-                ip = ip.split(",")[0];
-            }
+            ip = ip.split(",")[0];
         }
-        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip) || !isVaildIpAddr(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
             logger.info("Proxy-Client-IP ip: {}", ip);
         }
-        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip) || !isVaildIpAddr(ip)) {
             ip = request.getHeader("WL-Proxy-Client-IP");
             logger.info("WL-Proxy-Client-IP ip: {}", ip);
         }
-        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip) || !isVaildIpAddr(ip)) {
             ip = request.getHeader("HTTP_CLIENT_IP");
             logger.info("HTTP_CLIENT_IP ip: {}", ip);
         }
-        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip) || !isVaildIpAddr(ip)) {
             ip = request.getHeader("HTTP_X_FORWARDED_FOR");
             logger.info("HTTP_X_FORWARDED_FOR ip: {}", ip);
         }
-        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip) || !isVaildIpAddr(ip)) {
             ip = request.getHeader("X-Real-IP");
             logger.info("X-Real-IP ip: {}", ip);
         }
-        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip) || !isVaildIpAddr(ip)) {
             ip = request.getRemoteAddr();
             logger.info("getRemoteAddr ip: {}", ip);
         }
@@ -55,4 +54,23 @@ public class IpUtils {
         return ip;
     }
 
+    private static boolean isVaildIpAddr(String ip) {
+        return ip.matches(
+                "((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})(\\.((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})){3}")   //ipv4
+                ||ip.matches("([\\da-fA-F]{1,4}:){7}[\\da-fA-F]{1,4}");
+//        return IPAddressUtil.isIPv4LiteralAddress(ip) || IPAddressUtil.isIPv6LiteralAddress(ip);
+    }
+
+
+    public static void main(String[] args) {
+        IpUtils utils = new IpUtils();
+        System.out.println("utils.isVaildIpAddr(\"001.00.23.1\") = " + isVaildIpAddr("001.00.23.1"));
+        System.out.println("utils.isVaildIpAddr(\"00.0.0.0\") = " + isVaildIpAddr("00.0.0.0"));
+        System.out.println("utils.isVaildIpAddr(\"1.001.23.1\") = " + isVaildIpAddr("1.001.23.1"));
+
+        String ip = "0000";
+        String[] split = ip.split(",");
+        System.out.println("Arrays.toString(spli) = " + Arrays.toString(split));
+
+    }
 }
