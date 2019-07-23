e84e4b28475cb1717e4510b68f464067fc40f25a
yuan <sa517294@mail.ustc.edu.cn>
2019-07-23 14:17:38 +0800

    revise CheckServiceImpl.java

--- src/main/java/com/yanxue/service/impl/CheckServiceImpl.java
+++ src/main/java/com/yanxue/service/impl/CheckServiceImpl.java
@@ -1,5 +1,6 @@
 package com.yanxue.service.impl;
 
+import com.yanxue.util.CollectionUtils;
 import java.util.Date;
 import java.util.List;
 
@@ -9,10 +10,8 @@ import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSONArray;
 import com.yanxue.dao.CheckDao;
-import com.yanxue.dao.StudentDao;
 import com.yanxue.entity.Check;
 import com.yanxue.entity.JsonResult;
-import com.yanxue.entity.Lecture;
 import com.yanxue.entity.MyResult;
 import com.yanxue.entity.Party;
 import com.yanxue.entity.Student;
@@ -21,13 +20,13 @@ import com.yanxue.util.GUID;
 @Service
 public class CheckServiceImpl extends BaseServiceImpl<Check> {
     @Resource
-    CheckDao dao;
+    CheckDao checkDao;
     
     @Override
     public MyResult add(Check t) {
         try {
             //判断二维码是否失效
-            String status = dao.getStatus(t.getLecture_id());
+            String status = checkDao.getStatus(t.getLecture_id());
             if ("1".equals(status)) {
                 return new MyResult(31);
             }
@@ -37,27 +36,27 @@ public class CheckServiceImpl extends BaseServiceImpl<Check> {
             t.setStudent_number(number);
             
             //判断该学号是否存在
-            Party party = dao.getParty(number);
+            Party party = checkDao.getParty(number);
             if (party == null) {
                 return new MyResult(32);
             }
             
             //判断是否重复签到
-            Check check = dao.check(t.getStudent_number(), t.getLecture_id());
+            Check check = checkDao.check(t.getStudent_number(), t.getLecture_id());
             if (check != null) {
                 return new MyResult(2);
             }
             
             //判断一个IP是否提交多个信息
-            Check checkIp = dao.checkIp(t.getIp(), t.getLecture_id());
+            Check checkIp = checkDao.checkIp(t.getIp(), t.getLecture_id());
             if (checkIp != null) {
-                int i = dao.update(t);
+                int i = checkDao.update(t);
                 if (i > 0) {
                     return new MyResult(33);
                 }
             }
             
-            int i = dao.add(t);
+            int i = checkDao.add(t);
             if (i > 0) {
                 return new MyResult(1);
             }
@@ -70,48 +69,42 @@ public class CheckServiceImpl extends BaseServiceImpl<Check> {
     
 
     public JsonResult getAll(String lectureId) {
-        List<Student> list = dao.getStudents(lectureId);
-        int total = dao.getStudentsCount(lectureId);
-        JsonResult jr = new JsonResult();
-        jr.setRows(list);
-        jr.setTotal(total);
+        List<Student> list = checkDao.getStudents(lectureId);
+        int total = checkDao.getStudentsCount(lectureId);
+        JsonResult jr = new JsonResult(total, list);
         return jr;
     }
 
     public JsonResult getAwardStudents(String lectureId) {
-        List<Student> list = dao.getAwardStudents(lectureId);
-        int total = dao.getAwardStudentsCount(lectureId);
-        JsonResult jr = new JsonResult();
-        jr.setRows(list);
-        jr.setTotal(total);
+        List<Student> list = checkDao.getAwardStudents(lectureId);
+        int total = checkDao.getAwardStudentsCount(lectureId);
+        JsonResult jr = new JsonResult(total, list);
         return jr;
     }
 
     public JsonResult getIntentionStudents(String lectureId) {
-        List<Student> list = dao.getIntentionStudents(lectureId);
-        int total = dao.getIntentionStudentsCount(lectureId);
-        JsonResult jr = new JsonResult();
-        jr.setRows(list);
-        jr.setTotal(total);
+        List<Student> list = checkDao.getIntentionStudents(lectureId);
+        int total = checkDao.getIntentionStudentsCount(lectureId);
+        JsonResult jr = new JsonResult(total, list);
         return jr;
     }
 
     
 
     public MyResult getCheckData(String lecid, Date time) {
-        List<Check> list = dao.getCheckData(lecid, time);
-        dao.updateCheckDataStatus(lecid, time);
+        List<Check> list = checkDao.getCheckData(lecid, time);
+        checkDao.updateCheckDataStatus(lecid, time);
         return new MyResult(list);
     }
     
     public MyResult getCheckData_1(String lecid, Date time) {
-        List<Check> list = dao.getCheckData_1(lecid, time);
+        List<Check> list = checkDao.getCheckData_1(lecid, time);
         return new MyResult(list);
     }
 
     public MyResult addAward(String lecId, String stuId) {
         MyResult r = new MyResult();
-        int f = dao.addAward(GUID.get32UUID(), lecId, stuId);
+        int f = checkDao.addAward(GUID.get32UUID(), lecId, stuId);
         if (f > 0) {
             r.setStatus(1);
         } else {
@@ -123,15 +116,20 @@ public class CheckServiceImpl extends BaseServiceImpl<Check> {
     public JSONArray loadDataJson(String lectureId, String type) {
         JSONArray ja = new JSONArray();
         List<Student> list = null;
-        if ("attend".equals(type)) {
-            list = dao.getStudents(lectureId);
-        } else if ("award".equals(type)) {
-            list = dao.getAwardStudents(lectureId);
-        } else if ("intention".equals(type)) {
-            list = dao.getIntentionStudents(lectureId);
+        switch (type) {
+            case "attend":
+                list = checkDao.getStudents(lectureId);
+                break;
+            case "award":
+                list = checkDao.getAwardStudents(lectureId);
+                break;
+            case "intention":
+                list = checkDao.getIntentionStudents(lectureId);
+                break;
+            default:
         }
-        for (Student bean : list) {
-            ja.add(bean);
+        if (CollectionUtils.isNotEmpty(list)) {
+            ja.addAll(list);
         }
         return ja;
     }
